ef3fed30e9d2356afdb69d0fa86f391905ff9aea
swarnab99 <swarnab93@gmail.com>
2022-05-31 20:40:37 +0530

    update

--- utils/prismicClient.js
+++ utils/prismicClient.js
@@ -1,9 +1,9 @@
-import { PrismicLink } from 'apollo-link-prismic';
+import { createPrismicLink } from 'apollo-link-prismic';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import ApolloClient from 'apollo-client';
 
 const client = new ApolloClient({
-	link: PrismicLink({
+	link: createPrismicLink({
 		uri: `https://${process.env.PRISMIC_ID}.cdn.prismic.io/graphql`,
 		accessToken: process.env.PRISMIC_ACCESS_TOKEN,
 	}),
