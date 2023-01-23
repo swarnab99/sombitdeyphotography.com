c09108fe38f3f69afc9c773a3c53029b9b7eade7
Darpan Chakraborty <favfly.darpan@gmail.com>
2023-01-23 14:47:11 +0530

    update

--- components/layouts/Default.js
+++ components/layouts/Default.js
@@ -1,19 +1,19 @@
-import Header from '../header/Header';
-import Footer from '../footer/Footer';
-import WhatsappButton from '../WhatsappButton';
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+import WhatsappButton from "../WhatsappButton";
 
 const DefaultLayout = ({ children }) => {
-	return (
-		<main className='main-content'>
-			<Header />
-			<div className='main'>{children}</div>
-			<Footer />
-			<WhatsappButton
-				number='+917003793551'
-				text='Hi, Sombit Dey Photography'
-			/>
-		</main>
-	);
+  return (
+    <main className="main-content">
+      <Header />
+      <div className="main">{children}</div>
+      <Footer />
+      <WhatsappButton
+        number="+919830570102"
+        text="Hi, Sombit Dey Photography"
+      />
+    </main>
+  );
 };
 
 export default DefaultLayout;
